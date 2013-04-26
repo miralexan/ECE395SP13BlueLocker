3bba0ab59974a9fb8e9dc9eb19feebc8444f4eb3
Ethan Trovillion <trovill1@illinois.edu>
2013-04-25 20:31:10 -0500

    Added documentation to password.c

--- ARM_code/password.c
+++ ARM_code/password.c
@@ -4,16 +4,24 @@
 char password[512];
 char password_set;
 
+/* readpass
+ * Inputs: none
+ * Outputs: none
+ * Side-Effects:
+ *   Loads the current password state and hash from storage.
+ */
 void readpass() {
 	read_storage(&password_set, 1, 0);
-	if (password_set == 1) {
-		password_set = 1;
-	} else {
-		password_set = 0;
-	}
 	read_storage(password, HASH_LENGTH, 1);
 }
 
+/* setpass
+ * Inputs:
+ *   pass_in - A C-string containing the user password
+ * Outputs: none
+ * Side-Effects:
+ *   Generates a hash from the password and writes it to storage.
+ */
 void setpass(const char* pass_in) {
 	hashpass(pass_in, password);
 	password_set = 1;
@@ -21,15 +29,35 @@ void setpass(const char* pass_in) {
 	write_storage(password, HASH_LENGTH, 1);
 }
 
+/* checkpass
+ * Inputs: 
+ *   string - A C-string containing the user password
+ * Outputs:
+ *   Returns 1 if the supplied password generates a match to the
+ *	 stored hash, 0 if not.
+ * Side-Effects: None
+ */
 int checkpass(const char *string) {
 	if (password_set == 0) { return 0; }
 	return checkhash(string, password);
 }
 
+/* passisset
+ * Inputs: none
+ * Outputs:
+ *   Returns 1 if a password has been set, 0 if not
+ * Side-Effects: none
+ */
 int passisset() {
 	return password_set;
 }
 
+/* unsetpass
+ * Inputs: none
+ * Outputs: none
+ * Side-Effects:
+ *   Unsets the current password.
+ */
 void unsetpass() {
 	password_set = 0;
 	write_storage(&password_set, 1, 0);
