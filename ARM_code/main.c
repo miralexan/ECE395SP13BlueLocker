3be635cc9633901dd82cff09bf3f1e60bd2ee7b5
Ethan Trovillion <trovill1@illinois.edu>
2013-05-05 02:52:00 -0500

    Added author/project/course header to all group-written *.c and *.h files.

--- ARM_code/main.c
+++ ARM_code/main.c
@@ -1,3 +1,9 @@
+/* main.c
+ * Written by Ethan Trovillion and Ethan Warth
+ * Part of the BlueLock project
+ * ECE 395 Spring 2013
+ * University of Illinois Urbana-Champaign
+ */
 #include "bluelock.h"
 
 device flash;
